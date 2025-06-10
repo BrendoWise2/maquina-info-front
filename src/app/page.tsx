b7f5c80eeafcd8ccb9821c949da07023d83d2114
BrendoWise2 <brendo.oliveira@aluno.unincor.edu.br>
2025-06-09 21:58:38 -0300

    1.0v app

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+import styles from "./page.module.scss";
+import mecanimoImg from '/public/imagens/background.jpg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+
+    <>
+      <div className={styles.containerCenter}>
+
+
+        <section className={styles.login}>
+          <h1> Login</h1>
+          <form>
+            <input type="email" required name="email" placeholder="Digite seu email..." className={styles.input} />
+            <input type="password" required name="password" placeholder="**********" className={styles.input} />
+            <button type="submit">
+              Acessar
+            </button>
+
+            <Link href="/signup" className={styles.text}>
+              Não possui uma conta? cadastrar-se
+            </Link>
+
+          </form>
+        </section>
+      </div >
+
+
+    </>
+
+  );
+}
